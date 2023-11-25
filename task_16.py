e53fcc7839f4538c21474f6619b613df545a5267
Andrey <pustakovandrej@gmail.com>
2023-11-25 22:10:58 +0300

    add 16

--- task_16.py
+++ task_16.py
@@ -1,1 +1,13 @@
-print("Hellow world")
+print("Введите наруральное число n:")
+n = int(input())
+
+
+# Функция поиска значения F(n):
+def function_f(n):
+    if n <= 2:
+        return 1
+    else:
+        return function_f(n - 1) + 3 * function_f(n - 2)
+
+
+print("F(", n, ") =", function_f(n))
